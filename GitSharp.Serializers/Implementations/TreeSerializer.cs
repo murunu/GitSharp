2fff771a82ebbe0a58c69c98198f88d36d9d8dee
murunu <morenoschenk@gmail.com>
2025-02-28 22:47:42 +0100

    Implement TreeSerializer.cs

--- GitSharp.Serializers/Implementations/TreeSerializer.cs
+++ GitSharp.Serializers/Implementations/TreeSerializer.cs
@@ -1,5 +1,6 @@
 ﻿using GitSharp.Models;
 using GitSharp.Serializers.Interfaces;
+using OutParsing;
 
 namespace GitSharp.Serializers.Implementations;
 
@@ -7,11 +8,48 @@ internal class TreeSerializer : ITreeSerializer
 {
     public Tree Deserialize(string data)
     {
-        throw new NotImplementedException();
+        var tree = new Tree();
+
+        foreach (var line in data.Split(Environment.NewLine))
+        {
+            OutParser.Parse(
+                line,
+                "{mode} {type} {hash}\t{name}",
+                out string mode,
+                out string type,
+                out string hash,
+                out string name);
+
+            var fileType = Enum.Parse<FileType>(type, ignoreCase: true);
+            var modeType = Enum.Parse<Mode>(mode, ignoreCase: true);
+            
+            tree.TreeEntries.Add(
+                new TreeEntry
+                {
+                    Type = fileType,
+                    Hash = hash,
+                    Name = name,
+                    Mode = modeType
+                });
+        }
+
+        return tree;
     }
 
-    public string Serialize(Tree obj)
+    public string Serialize(Tree tree)
     {
-        throw new NotImplementedException();
+        List<string> strings = [];
+
+        foreach (var treeEntry in tree.TreeEntries)
+        {
+            var mode = ((int)treeEntry.Mode).ToString();
+            if (mode.Length < 6)
+            {
+                mode = mode.PadLeft(6, '0');
+            }
+            strings.Add($"{mode} {treeEntry.Type.ToString().ToLower()} {treeEntry.Hash}\t{treeEntry.Name}");
+        }
+
+        return string.Join(Environment.NewLine, strings);
     }
 }
